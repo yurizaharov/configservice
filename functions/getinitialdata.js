347e94078e11b88116daecc0144e6fd598fcdaa7
Yuri Zakharov <yuri.zaharov@gmail.com>
2021-03-14 21:01:17 +0300

    Changed to use configservice

--- functions/getinitialdata.js
+++ functions/getinitialdata.js
@@ -1,25 +1,20 @@
-const fs = require('fs');
-const configFolder = './configs/';
+const axios = require('axios');
 
-function getinitialdata() {
+// Setting variables
+initialDataURL = process.env.CONFIGSERVICE_ADDR || 'localhost:8080'
+
+async function getinitialdata() {
     let initialData = [];
 
-    fs.readdirSync(configFolder).forEach(file => {
-        let arrayOfParts = file.split(".");
-        let dataBase = arrayOfParts[0]
-        let rawData = fs.readFileSync(configFolder+'/'+file);
-        let Data = JSON.parse(rawData);
-        initialData.push({
-            "dataBase" : dataBase,
-            "user" : Data.user,
-            "password" : Data.password,
-            "connectString" : Data.connectString,
-            "processingExt" : Data.processingExt,
-            "processingInt" : Data.processingInt,
-            "mobileExt" : Data.mobileExt,
-            "mobileInt" : Data.mobileInt
-        })
-    });
+    try {
+        initialData = await axios.get('http://' + initialDataURL + '/api/configs/liquicheck')
+            .then((response) => {
+                return response.data;
+            });
+    } catch (err) {
+        console.log(err)
+    }
+
     return initialData;
 }
 
